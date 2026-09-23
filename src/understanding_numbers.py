f7b7787e9e812d934c38429f5fb5b7f0ddf05b91
Oskar Tadeo <2630002@upv.edu.mx>
2026-09-23 08:55:56 -0600

    en este commit aprendimos sobre la sintaxis y error de sintaxis y sobre las operaciones numericas usando numeros enteros

---
+++ src/understanding_numbers.py
@@ -0,0 +1,25 @@
+#Numeros
+# Enteros - Integers
+"""
+Los numeros enteros los podemos sumar(+), restar (-),
+multiplicar (*) y/o dividir (/).
+"""
+#Operaciones con enteros
+print("\nOperaciones con enteros")
+print(2+3)
+print(3-2)
+print(2*3)
+print(3/2)
+
+#Usando con variables de tipo enteras
+print("\nOperaciones variables tipo enteras")
+number_1 = 5
+number_2 = 10
+print(number_1+number_2)
+
+#Operaciones con decimales
+print("\nOperaciones con decimales")
+print(2.1+1.5)
+print(3.5-0.9)
+print(2*5.1)
+print(1.5/2)
---
+++ src/understanding_sintax_error.py
@@ -0,0 +1,17 @@
+programming_lenguage = "     python     "
+print(programming_lenguage)
+print("Efecto del lstrip")
+print(programming_lenguage.lstrip())
+print("Efecto del rstrip")
+print(programming_lenguage.rstrip())
+print("Efecto del strip")
+print(programming_lenguage.strip())
+print("variable original")
+print(programming_lenguage)
+
+#Error de sintaxis
+"""
+message = 'Una fortaleza de 'python' es su comunidad'
+print(message)
+
+"""
