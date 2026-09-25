39c448b9db2de894767d202cefb3d1a6bb7ef1c7
Oskar Tadeo <2630002@upv.edu.mx>
2026-09-25 08:57:52 -0600

    En este commit, actualizamos este archivo incluyendo numeros flotantes, build-in type() y el type error

--- src/understanding_numbers.py
+++ src/understanding_numbers.py
@@ -19,7 +19,46 @@ print(number_1+number_2)
 
 #Operaciones con decimales
 print("\nOperaciones con decimales")
-print(2.1+1.5)
-print(3.5-0.9)
-print(2*5.1)
-print(1.5/2)
+print(2.1 + 1.5)
+print(3.5 - 0.9)
+print(2 * 5.1)
+print(1.5 / 2)
+
+# Enteros
+# Sumar +
+# Restar -
+# Multiplicar *
+# Dividir /
+# Dividir enteros //n
+# Potencias **n
+print("\nExponentes")
+print(3 **2) # 3^2
+print(3 **3) # 3^3
+print(10 **6) # 10^6
+print(10 %2) # Modulo (mod)
+
+print("\nDecimales o numeros de flotantes")
+print(0.1 + 0.1)
+print(0.2 - 0.2)
+print(2 * 0.1)
+print(2 * 0.2)
+
+# Imprimir la edad de alguien
+age = 34 # Variable del tipo init
+# message = "Charly tiene " + age + " años" (TYPEERROR)
+print("\nusando 'str()'")
+message = "Charly tiene " + str(age) + " años"
+print(message)
+
+# Type Error
+
+"""
+    Type Error: Python no reconoce el tipo de informacion que se está utilizando
+"""
+print("\nusando 'f string'")
+message_f = f"Charly tiene {age} años"
+print(message_f)
+
+# Metodo build-in type()
+
+print(type(age), type("hola"), type(0.25), type(True))
